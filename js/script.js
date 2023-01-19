9c3ea0a9d28154d75daa119abf13c50ee8aa0bb5
Cloud <65218174+cloudkasalu@users.noreply.github.com>
2023-01-19 03:16:59 +0200

    new-updates

--- js/script.js
+++ js/script.js
@@ -21,3 +21,28 @@ navbarToggler.addEventListener('click', (e)=>{
          
 
 })
+
+
+
+
+document.addEventListener("click", function(e){
+  console.log("active")
+  const isDropdownButton = e.target.matches('[data-dropdown-button]');
+  if(!isDropdownButton && e.target.closest('[data-dropdown]') != null ) return
+
+ let currentDropdown;
+ if(isDropdownButton){
+  currentDropdown = e.target.closest('[data-dropdown]');
+  currentDropdown.classList.toggle('active');
+ }
+
+ document.querySelectorAll('[data-dropdown].active').forEach(dropdown =>{
+  if(dropdown == currentDropdown){
+    return
+  }else{
+    dropdown.classList.remove('active')
+  }
+ })
+
+
+})
